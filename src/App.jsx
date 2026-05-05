9a308d788af1913f04008e98a1d2626037ab5c7b
s-arbelaez <sofiaarme@unisabana.edu.co>
2026-05-05 11:03:53 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Catalogue from './pages/Catalogue'
-import SaleHistory from './pages/SaleHistory'
-import Cart from './pages/Cart'
-import Payment from './pages/Payment'
-import NewProduct from './pages/newProduct'
-import EditProduct from './pages/EditProduct'
-import Purchase from './pages/Purchase'
-import Providers from './pages/Providers'
-import Clients from './pages/Clients'
-import Categories from './pages/Categories'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/salehistory" element={<SaleHistory />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path='/newProduct' element={<NewProduct/>} />
-        <Route path="/purchase" element={<Purchase />} />
-        <Route path="/editProduct/:id" element={<EditProduct />} />
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/providers" element={<Providers/>} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
 
-export default App  
+export default App
---
+++ src/routes/index.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Catalogue from '../pages/Catalogue'
+import SaleHistory from '../pages/SaleHistory'
+import Cart from '../pages/Cart'
+import Payment from '../pages/Payment'
+import NewProduct from '../pages/NewProduct'
+import EditProduct from '../pages/EditProduct'
+import Purchase from '../pages/Purchase'
+import Providers from '../pages/Providers'
+import Clients from '../pages/Clients'
+import Categories from '../pages/Categories'
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogue" element={<Catalogue />} />
+            <Route path="/salehistory" element={<SaleHistory />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/newProduct" element={<NewProduct />} />
+            <Route path="/purchase" element={<Purchase />} />
+            <Route path="/editProduct/:id" element={<EditProduct />} />
+            <Route path="/clients" element={<Clients />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/providers" element={<Providers />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+    )
+}
